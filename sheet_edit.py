49a36c5a739ac582f5c370b43b5bb9a472bdd48b
Arpit Gothi <agothi@splunk.com>
2023-03-08 00:48:14 +0530

    Addinf multi threading feature

--- sheet_edit.py
+++ sheet_edit.py
@@ -1,6 +1,7 @@
 import argparse
 from operator import length_hint
 import sys
+from threading import Thread
 from urllib.error import HTTPError
 from bs4 import BeautifulSoup
 import os
@@ -12,7 +13,7 @@ from urllib.parse import parse_qs, urlparse
 from jira.client import JIRA
 from google.oauth2 import service_account
 from googleapiclient.discovery import build
-from datetime import datetime as dt
+from datetime import datetime, timedelta
 
 AD_USER = getuser()
 now = time.time()
@@ -46,10 +47,16 @@ except Exception as e:
 
 
 def suffix(d):
-    return 'th' if 11<=d<=13 else {1:'st',2:'nd',3:'rd'}.get(d%10, 'th')
+    return 'th' if 11 <= d <= 13 else {1:'st',2:'nd',3:'rd'}.get(d % 10, 'th')
+
+
 
 def custom_strftime(format, t):
+    if t.time() < datetime.strptime('06:30:00', '%H:%M:%S').time():
+        # If the time is before 6:30 AM, use yesterday's date
+        t -= timedelta(days=1)
     return t.strftime(format).replace('{S}', str(t.day) + suffix(t.day))
+
 
 def find_TO(JIRA_ID):
     query = 'project = "TechOps" AND text ~ "' + JIRA_ID +'"'
@@ -63,11 +70,6 @@ def getJiraStatus(JIRA_ID):
     print(JIRA_ID,jira.issue(to).raw["fields"]["status"]["name"], to)
     return jira.issue(to).raw["fields"]["status"]["name"]
     
-
-def updateSheet():
-    
-    pass
-
 
 def conditional_formatting(spreadsheet_id):
     """
@@ -148,10 +150,15 @@ def conditional_formatting(spreadsheet_id):
 
 
 output = []
-#outputt = []
+
+def process_jira_id(jira_id, current_status):
+    status = getJiraStatus(jira_id)
+    current_status.update( {jira_id : status} )
+
+
 
 def main():
-    sheet_name= custom_strftime('{S} %b', dt.now())
+    sheet_name= custom_strftime('{S} %b', datetime.now())
     print(sheet_name)
     
 
@@ -165,45 +172,63 @@ def main():
 
     # The ID and range of a sample spreadsheet.
     SAMPLE_SPREADSHEET_ID = '1kq5i69XwvTJBRkLAb47LTkGV-CE3-oxTDPU4Qh5xBBc'
-    SAMPLE_RANGE_NAME = '16th Oct!B2:B'
 
     service = build('sheets', 'v4', credentials=creds)
 
     # Call the Sheets API
     sheet = service.spreadsheets()
-    result = sheet.values().get(spreadsheetId=SAMPLE_SPREADSHEET_ID,
+    job_sheet = sheet.values().get(spreadsheetId=SAMPLE_SPREADSHEET_ID,
                                 range=f"{sheet_name}!B2:B").execute()
-    length = len(result['values'])
-    result_H = sheet.values().get(spreadsheetId=SAMPLE_SPREADSHEET_ID,
+    length = len(job_sheet['values'])
+    sheet_state = sheet.values().get(spreadsheetId=SAMPLE_SPREADSHEET_ID,
                                 range=f"{sheet_name}!H2:H").execute()
-    print(result_H["values"])
-    #print(result.get('values', []))
-    if length == len(result_H['values']):
-        if ''.join(result_H['values'][length-1]).lower() == "closed":
+    print("\nStatus on sheet: ", sheet_state["values"])
+    #print(job_sheet.get('values', []))
+    if length == len(sheet_state['values']):
+        if ''.join(sheet_state['values'][length-1]).lower() == "closed":
             sys.exit("\n\nScript exited. As its fully updated !!!")
     k=0
-    #print(result['values'])
-    for i in result['values']:
-        print()
-        try:
-            #print(tmp,k,result_H['values'][k])
-            if ''.join(result_H['values'][k]).lower() in ("closed", "job-completed"):
-                print(f"{i[0]} is already closed!")
-                tmp = ''.join(result_H['values'][k])
-            else:
-                print(f"checking Jira Status for {i[0]}")
-                tmp = getJiraStatus(i[0])
-        except:
-            pass
-        #print(tmp.split(","))
-        output.append(tmp.split())
-        #outputt.append(tmp.split())
-        k=k+1
-        #print(output)
+    print("\nJOBs: ", job_sheet['values'])
+    
+    
+    #$%% for i in job_sheet['values']:
+    #$%%     print()
+    #$%%     try:
+    #$%%         #print(tmp,k,sheet_state['values'][k])
+    #$%%         if ''.join(sheet_state['values'][k]).lower() in ("closed", "job-completed"):
+    #$%%             print(f"{i[0]} is already closed!")
+    #$%%             tmp = ''.join(sheet_state['values'][k])
+    #$%%         else:
+    #$%%             print(f"checking Jira Status for {i[0]}")
+    #$%%             tmp = getJiraStatus(i[0])
+    #$%%     except:
+    #$%%         pass
+    #$%%     #print(tmp.split(","))
+    #$%%     output.append(tmp.split())
+    #$%%     k=k+1
+    #$%%     #print(output)
+#
         
-    print(output)
-    #print(outputt)
+    # Process JIRA IDs in parallel
+    jira_ids = [row[0] for row in job_sheet.get('values', [])]
+    threads = []
+    current_status = {}
+    for jira_id in jira_ids:
+        thread = Thread(target=process_jira_id, args=(jira_id, current_status))
+        thread.start()
+        threads.append(thread)
+
+    for thread in threads:
+        thread.join()
+    
+    # Update spreadsheet with job_sheets
+    for job in job_sheet['values']:
+        output.append([current_status[job[0]]])
+    
+    #values = [[status] for status in output]
+    print("\nCurrent Status: ", output)
     sheet = service.spreadsheets()
-    result = sheet.values().update(spreadsheetId=SAMPLE_SPREADSHEET_ID, range=f"{sheet_name}!H2", valueInputOption="USER_ENTERED", body={"values":output}).execute()
+    job_sheet = sheet.values().update(spreadsheetId=SAMPLE_SPREADSHEET_ID, range=f"{sheet_name}!H2", valueInputOption="USER_ENTERED", body={"values":output}).execute()
+    print("Sheet Updated")
 
 main()
